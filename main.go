a099719bbca8587b74e87fca68e8d29e2474d8a6
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2017-03-11 21:34:42 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"log"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"os"
+	"net/http"
+	"encoding/json"
+	"fmt"
+        "crypto/sha256"
+	"strings"
+	"io"
+)
+
+type Response struct {
+	status  int
+	Message string
+	Data    interface{}
+}
+
+type paramRequest struct {
+	Application string
+	Environment string
+	Version     string
+	Landscape   string
+}
+
+func (p paramRequest) valid() bool {
+	if p.Application == "" || p.Environment == "" || p.Version == "" || p.Landscape == "" {
+		return false
+	}
+	return true
+}
+
+func (p paramRequest) envPrefix() string {
+	return fmt.Sprintf("%s.%s.%s", p.Landscape, p.Environment, p.Application)
+}
+
+func (p paramRequest) cacheKey() string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(p.identifier())))
+}
+
+func (p paramRequest) identifier() string {
+	return fmt.Sprintf("%s@%s", p.envPrefix(),p.Version)
+}
+
+var (
+	CACHE = make(map[string]Response)
+)
+
+func main() {
+	api()
+}
+
+func api() {
+	router := mux.NewRouter().StrictSlash(true)
+	registerHandlers(router)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
+	log.Println("Validating Config") //todo, validate config
+	log.Println("Started: Ready to serve")
+	log.Fatal(http.ListenAndServe(":8080", loggedRouter)) //todo, refactor to make port dynamic
+}
+
+func registerHandlers(r *mux.Router) {
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	r.HandleFunc("/params", envHandler).Methods("POST")
+
+}
+
+func paramRequestBody(b io.ReadCloser)  paramRequest{
+	decoder := json.NewDecoder(b)
+	var p paramRequest
+	err := decoder.Decode(&p)
+	if err != nil {
+		log.Printf("encountered issue decoding request body; %s",err.Error())
+		return paramRequest{}
+	}
+	return  p
+}
+
+func (p paramRequest) getData()  map[string]string{
+	c := ssmClient{NewClient("us-east-2")}
+	paramNames := c.WithPrefix(p.envPrefix())
+	return paramNames.IncludeHistory(c).withVersion(p.Version) //todo, return error
+}
+
+func envHandler(w http.ResponseWriter, r *http.Request) {
+	p := paramRequestBody(r.Body)
+	if !p.valid() {
+		badRequest(w, p)
+		return
+	}
+	log.Printf("Processing request for %s uniquely identified as %+v", p.identifier(),p.cacheKey())
+	cached,ok := CACHE[p.cacheKey()]
+	if ok {
+		log.Printf("Retrieved parameters from cache")
+		JSONResponseHandler(w, cached)
+		return
+	}
+	resp := Response{status:http.StatusOK, Data:p.getData()} //todo, check length of list before returning
+	CACHE[p.cacheKey()] = resp //todo, dont cache if empty
+	JSONResponseHandler(w, resp)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var m = make(map[string]string)
+	m["error"] = fmt.Sprintf("Route %s not found with method %s, please check request and try again",
+		r.URL.Path,r.Method)
+	resp := Response{Data:m, status:http.StatusNotFound}
+	JSONResponseHandler(w, resp)
+}
+
+func badRequest(w http.ResponseWriter, p paramRequest) {
+	w.Header().Set("Content-Type", "application/json")
+	var m = make(map[string]string)
+	expected := strings.ToLower(fmt.Sprintf("%+v", paramRequest{"STRING", "STRING", "STRING", "STRING"}))
+	m["error"] = fmt.Sprintf("Bad request, expected: %s, got: %s", expected, strings.ToLower(fmt.Sprintf("%+v", p)))
+	resp := Response{Data:m, status:http.StatusBadRequest}
+	JSONResponseHandler(w, resp)
+}
+
+func JSONResponseHandler(w http.ResponseWriter, resp Response) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(resp.status)
+	json.NewEncoder(w).Encode(resp.Data)
+}
---
+++ param.go
@@ -0,0 +1,136 @@
+package main
+
+import "fmt"
+import (
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"log"
+	"strings"
+	"errors"
+)
+
+type ssmClient struct {
+	client *ssm.SSM
+}
+
+type parameter struct {
+	Name string
+	Versions []paramHistory
+
+}
+
+type parameters []parameter
+
+type paramHistory struct {
+	Value string
+	Version string
+}
+
+func NewClient(region string) *ssm.SSM {
+	session := session.Must(session.NewSession())
+	session.Config.WithRegion(region)
+	return ssm.New(session)
+}
+
+func (s ssmClient) WithPrefix(prefix string) parameters{
+	var names parameters
+	resp,err := s.ParamList(prefix)
+	if err != nil {
+		log.Println("Encountered an error listing params")
+		return parameters{}
+	}
+	for _,param := range resp.Parameters {
+		names = append(names,parameter{*param.Name,[]paramHistory{}})
+	}
+	return names
+}
+
+func (s ssmClient) ParamList(filter string) (*ssm.DescribeParametersOutput, error) {
+	//limit of 50 parameters, unless extra logic is added to paginate
+	params := &ssm.DescribeParametersInput{
+		MaxResults: aws.Int64(50),
+		Filters: []*ssm.ParametersFilter{
+			{
+				Values: []*string{
+					aws.String(filter),
+				},
+				Key: aws.String("Name"),
+			},
+		},
+	}
+	return s.client.DescribeParameters(params)
+}
+
+func (p parameters) IncludeHistory(s ssmClient) parameters{
+	var params parameters
+	for _,param := range p {
+		param.history(s)
+		params = append(params,param)
+	}
+	return params
+}
+
+func (p *parameter) history(s ssmClient) {//todo, return error
+	pi := &ssm.GetParametersInput{
+		Names: []*string{&p.Name},
+		WithDecryption: aws.Bool(true),
+	}
+	hpi := &ssm.GetParameterHistoryInput{
+		Name: &p.Name,
+		WithDecryption: aws.Bool(true),
+	}
+	resp, err := s.client.GetParameterHistory(hpi)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	r, err := s.client.GetParameters(pi)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	re, err := s.ParamList(p.Name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	//todo, guard against empty param
+	p.Versions = append(p.Versions,paramHistory{Value:*r.Parameters[0].Value,Version:*re.Parameters[0].Description})
+	var hist []paramHistory
+	var des string
+	for _,param := range resp.Parameters {
+		if param.Description != nil{
+			des = *param.Description
+
+		}
+		val := *param.Value
+		hist = append(hist,paramHistory{Value:val,Version:des })
+	}
+	p.Versions = append(p.Versions,hist...)
+	return
+}
+
+func (p parameters) withVersion(version string) map[string]string{
+	paramsDoc := make(map[string]string)
+	for _,param := range p {
+		ver,err := param.containsVersion(version)
+		if err != nil {
+			log.Printf("Error: could not find version: %v for param %v",version,param.Name)
+			continue
+		}
+		ParsedName := strings.Split(param.Name,".") //todo, check if envName matches ENV VAR regex
+		envName := ParsedName[len(ParsedName) - 1]
+		paramsDoc[envName] = ver.Value
+	}
+	return paramsDoc
+}
+
+func (p parameter) containsVersion(version string) (paramHistory,error) {
+	for _,v := range p.Versions {
+		if v.Version == version {
+			return v,nil
+		}
+	}
+	return paramHistory{},errors.New("Could not find version")
+}
