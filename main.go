190c68147bbcb16f659fde8cb9febdb273944931
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2017-06-08 05:39:23 -0400

    re-pinned region

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func (p paramRequest) identifier() string {
 
 var (
 	CACHE = make(map[string]Response)
-	region = os.Getenv("AWS_REGION")
+	region = "us-east-2"//os.Getenv("AWS_REGION")
 )
 
 func main() {
